d3ece241efe9ceb4e58aadc808e16291c113834c
IbraevSergey <ibraevs.f@inbox.ru>
2018-09-12 01:35:02 +0300

    Отформатирован код

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -4,11 +4,11 @@ public class Square {
 
   public double l;
 
-  public Square (double l){
+  public Square(double l) {
     this.l = l;
   }
 
-  public  double area() {
+  public double area() {
     return this.l * this.l;
   }
 }
